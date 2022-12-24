25a345e827716e5c4679547d2bfe879742fe879d
pnk2ponkotsu <111572219+pnk2ponkotsu@users.noreply.github.com>
2022-12-24 13:31:32 +0000

    [controller作成]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'top' => 'homes#top'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'users/show'
+  get 'users/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
